59a966b245d73131c226377af7bb9fd049189689
Dino Prašo <desmond1303@gmail.com>
2014-05-31 15:18:01 +0200

    Added Most of the Stuff

--- Program.cs
+++ Program.cs
@@ -17,43 +17,110 @@ namespace StudentClassHW
     {
         protected string name, lastName;
 
-        Person(string name, string lastName)
+        protected Person(string name, string lastName)
         {
             this.name = name;
             this.lastName = lastName;
         }
-        
-        public string getPersonInfo() { return name + ", " + lastName; }
+
+        protected string getPersonInfo()
+        {
+            return name + ", " + lastName;
+        }
     }
     
     class Student : Person, IComparable
     {
         public string email {get; set;}
-        private string location;
-        
+        private string location
+        {
+            get
+            {
+                if (location == "SA")
+                {
+                    return "Sarajevo";
+                }
+                else if (location == "TZ")
+                {
+                    return "Tuzla";
+                }
+                else
+                {
+                    return "Other";
+                }
+            }
+            set
+            {
+                if (value == "SA" || value == "SARAJEVO" || value == "Sarajevo")
+                {
+                    location = "Sarajevo";
+                }
+                else if (value == "TZ" || value == "TUZLA" || value == "Tuzla")
+                {
+                    location = "Tuzla";
+                }
+                else
+                {
+                    location = "NA";
+                }
+            }
+        }
+
+        public Student() : base("Dino", "Prašo")
+        {
+            this.email = "email@aubih.edu.ba";
+            this.location = "Sarajevo";
+        }
+
+        ~Student() {
+
+        }
+
+        public Student(string name, string lastname, string email)
+            : base(name, lastname)
+        {
+            this.email = email;
+            this.location = "Sarajevo";
+        }
+
         public bool setName(string input)
         {
-            if (.............)
+            if (input.Length <= 2)
             {
                 Console.WriteLine("Name must be at least two characters long");
                 return false;
             }
-            
-            if (................)
-            {
-                Console.WriteLine("Name can only have letters");
+
+            char[] cArray = input.ToCharArray();
+            foreach(char c in cArray) {
+                if(!char.IsLetter(c)) {
+                    Console.WriteLine("Name can only have letters");
                 return false;
+                }
             }
-            
-            if (..................)
+
+            if (char.IsUpper(cArray[0]))
             {
                 Console.WriteLine("Name must start with an uppercase letter");
                 return false;
             }
+
+            return true;
         }
         
         public string getStudentInfo()
         {
+            return base.getPersonInfo() + ", " + this.email + ", " + this.location;
+        }
+
+        public override string ToString()
+        {
+            return getStudentInfo();
+        }
+
+        public int CompareTo(Student student)
+        {
+            return location.CompareTo(student.location);
         }
     }
 }
